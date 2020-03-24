d17aee208d33f8a0a63244385c5d368c68bffc56
Barracoder <emmnauelobarany@gmail.com>
2020-03-24 22:35:14 +0300

    Fixed static files issue

--- Stass/settings.py
+++ Stass/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -137,3 +135,5 @@ import dj_database_url
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
